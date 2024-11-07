1910eb184088328224a22584ee9c87af9afbfc41
Martin Winkler <martin.winkler@fh-hagenberg.at>
2024-11-07 20:29:13 +0100

    v08

--- pointers/pointers.c
+++ pointers/pointers.c
@@ -102,7 +102,48 @@ void test_function_pointer(void) {
   }
 }
 
+typedef struct address { 
+  char street[40]; // NOTE: must contain size or be pointer
+  int nr;
+  int zip;
+  char *city; // NOTE: for pointer, only address is copied
+} address_t; 
+
+void print_address(address_t a) { 
+  printf("%s %d\n%d %s\n", a.street, a.nr, a.zip, a.city);
+}
+
 void test_struct(void) {
+  address_t a;
+  strncpy(a.street, "Softwarepark", ALEN(a.street));
+  a.nr = 11;
+  a.zip = 4232;
+  a.city = malloc(sizeof(*a.city) * 50);
+  if (a.city == NULL) {
+    printf("out of memory!\n");
+    return;
+  }
+  strncpy(a.city, "Hagenberg", 50);
+  print_address(a);
+
+  printf("copying address...\n");
+  address_t a2 = a; // NOTE: copied
+  print_address(a2);
+
+  printf("updating address 1...\n");
+  // strncpy(a.street, "Hauptplatz", 40);
+  // a.nr = 1;
+  // a.zip = 4020;
+  strncpy(a.city, "LINZ!", 50); // NOTE: city now both Linz
+
+  printf("address 1:\n");
+  print_address(a);
+
+  printf("address 2 (implicitly updated through common pointer):\n");
+  print_address(a2);
+
+  free(a.city); // NOTE: always free allocated memory
+  printf("sizeof(a) = %zu", sizeof(a));
 }
 
 

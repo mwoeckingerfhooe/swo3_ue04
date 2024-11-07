77f13ba7e66d628675a44d3c1a155e08e3a186ef
Martin Winkler <martin.winkler@fh-hagenberg.at>
2024-11-07 20:29:11 +0100

    v01

---
+++ list/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "list.h"
+
+#define DEBUG(X) printf("%s -> %d\n", (#X), (X))
+
+int main(void) {
+
+  return 0;
+}
---
+++ pointers/pointers.c
@@ -0,0 +1,58 @@
+#include <stdlib.h> // for malloc & free
+#include <stdio.h>
+#include <string.h> // for strlcpy
+#include "utils.h"
+
+#define LENGTH 50
+
+void test_malloc(void) {
+}
+
+void print_char_array(char s[]) {
+  UNUSED(s);
+}
+
+void print_char_pointer(char *s) {
+  UNUSED(s);
+}
+
+void test_print(void) {
+}
+
+int my_strncmp(char *a, char *b , int maxlen) {
+  UNUSED(a); UNUSED(b); UNUSED(maxlen);
+  return 0;
+}
+
+
+void test_my_strncmp(void) {
+}
+
+void print_array_of_pointers(const char *a[], int len) {
+  UNUSED(a); UNUSED(len);
+}
+
+void test_array_of_pointers(void) {
+}
+
+void test_pointer_of_pointer(void) {
+}
+
+void test_function_pointer(void) {
+}
+
+void test_struct(void) {
+}
+
+
+int main(void) {
+  test_malloc();
+  test_print();
+  test_my_strncmp();
+  test_array_of_pointers();
+  test_pointer_of_pointer();
+  test_function_pointer();
+  test_struct();
+
+  return 0;
+}

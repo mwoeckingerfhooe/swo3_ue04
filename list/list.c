31bc13375014daeb8ce68bf87c1b012e48f9b62b
Martin Winkler <martin.winkler@fh-hagenberg.at>
2024-11-07 20:29:13 +0100

    v10

--- list/list.c
+++ list/list.c
@@ -3,3 +3,15 @@
 #include <assert.h>
 
 #include "list.h"
+
+typedef struct node {
+  int value;
+  struct node *next;
+} node_t;
+
+// NOTE: definition of struct list
+// already typedef'd to list_t
+struct list {
+  node_t *first;
+  int n;
+};
--- list/list.h
+++ list/list.h
@@ -1,1 +1,7 @@
 #pragma once
+
+#include <stdbool.h>
+
+// NOTE: declare that list_t is short for struct list_t
+// it does not need to be known outside what it looks like
+typedef struct list list_t;
